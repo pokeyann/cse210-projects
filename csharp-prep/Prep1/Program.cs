957e3663dd99987ee10264e0758a360acd8d923a
pokeyann <pokeyann@gmail.com>
2024-01-13 16:55:31 -0600

    name activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,6 +8,7 @@ class Program
 
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
+
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
