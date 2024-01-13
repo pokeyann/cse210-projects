a2daf02ecf0648cb8a7e8e1f488f87d3fb557311
pokeyann <pokeyann@gmail.com>
2024-01-13 17:22:46 -0600

    Step 1 for prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.IO.Pipes;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string gradeFromUser = Console.ReadLine();
+
+        int grade = int.Parse(gradeFromUser);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }
+//A >= 90
+//B >= 80
+//C >= 70
+//D >= 60
+//F < 60
