72d0f292613967bc6e022dd0806a8dc45497e948
pokeyann <pokeyann@gmail.com>
2024-01-20 19:58:23 -0600

    user number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+        } while (userNumber != 0);
+
+        /* add userNumber to list numbers*/
+
+
     }
 }
