2f36417f5ba6902788495fac05431e698bd215d8
pokeyann <pokeyann@gmail.com>
2024-01-20 18:00:27 -0600

    step 3 random generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,10 +6,13 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
 
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
-
+        /*Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());*/
+        Console.WriteLine("Guess the magic number game");
         int guess = -1;
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
 
         do
         {
