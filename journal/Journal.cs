a8352f4083a40bbacb9401349fed02bf7219d1dc
pokeyann <pokeyann@gmail.com>
2024-01-26 19:09:44 -0600

    journaal

---
+++ journal/Journal.cs
@@ -0,0 +1,59 @@
+using System;
+
+/*
+1. Write a new entry - Show the user a random prompt (from a list that you create), and save their response, the prompt, 
+  and the date as an Entry.
+2. Display the journal - Iterate through all entries in the journal and display them to the screen.
+3. Save the journal to a file - Prompt the user for a filename and then save the current journal (the complete list of entries) to that file location.
+4. Load the journal from a file - Prompt the user for a filename and then load the journal (a complete list of entries) from that file. This should replace any entries currently stored the journal.
+5. Provide a menu that allows the user choose these options
+6. Your list of prompts must contain at least five different prompts.
+
+1. Contain classes for the major components in the program.
+2. Contain at least two classes in addition to the Program class.
+3. Demonstrate the principle of abstraction by using member variables and methods appropriately.
+*/
+/*1st class*/
+class Journal
+{
+  /*
+  Main menu
+  Please select on of the following choices:
+  1. Write
+  2. Display
+  3  Load
+  4. Save
+  5. Quit
+  What would you like to do?
+  */
+
+  /*
+  List
+  Load
+  Save
+  */
+
+}
+
+/*2nd class*/
+class Entry
+{
+  /*
+
+  Write: Get prompt from random generator
+  user input to answer prompt
+  save entry with prompt and date
+  add entry to list with prompt and date
+  Display*/
+
+
+}
+
+/*3rd class*/
+class Prompt
+{
+  /*
+  List of prompts
+  randomgenerator
+  */
+}
