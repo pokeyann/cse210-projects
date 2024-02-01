bc0ae392b44a6072e85bca295bab2cd695b53e6f
pokeyann <pokeyann@gmail.com>
2024-02-01 13:15:52 -0600

    prompt questions random generator

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -2,11 +2,23 @@ using System;
 
 public class Prompt
 {
-
-  List<string> promptQuestion = new List<string>
+  public void promptQuestions()
   {
+    List<string> questions = new List<string>();
 
-  };
+    questions.Add("What was the most surprising thing that happened today? ");
+    questions.Add("What am I most proud of? ");
+    questions.Add("What is my favorite animal and why? ");
+    questions.Add("One thing I want others to know is? ");
+    questions.Add("How do I feel about today? ");
 
-  Random prompt new Random();
+    Random prompt = new Random();
+
+    int randomIndex = prompt.Next(0, questions.Count - 1);
+  }
+
+
 }
+
+
+
