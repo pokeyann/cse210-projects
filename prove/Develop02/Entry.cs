cb6416554aff54321e2eb1dcdfdd8cafc227c6af
pokeyann <pokeyann@gmail.com>
2024-02-18 22:04:24 -0600

    Save and Read file

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,20 +1,62 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 public class Entry
 {
   public void displayEntry()
   {
+    string input = Console.ReadLine();
+
     List<string> entry = new List<string>();
 
-    string userEntry = Console.ReadLine();
-
-    entry.Add(userEntry);
+    entry.Add(input);
 
     foreach (string e in entry)
     {
       Console.WriteLine(e);
     }
 
+    SaveToFile(entry);
+
+    List<string> newEntry = ReadFromFile();
+    foreach (string n in newEntry)
+    {
+      Console.WriteLine(n);
+    }
+
+  }
+
+  public static void SaveToFile(List<string> entry)
+  {
+    Console.WriteLine("testing save to file...");
+    string filename = "journal.txt";
+
+    using (StreamWriter outputFile = new StreamWriter(filename))
+    {
+      foreach (string e in entry)
+      {
+        outputFile.WriteLine(e);
+      }
+    }
+
+  }
+
+  public static List<string> ReadFromFile()
+  {
+    Console.WriteLine("testing read from file...");
+    List<string> entry = new List<string>();
+
+    string filename = "journal.txt";
+
+    string[] lines = System.IO.File.ReadAllLines(filename);
+
+    foreach (string line in lines)
+    {
+      Console.WriteLine(line);
+    }
+
+    // need to add .Add
+    return entry;
   }
 }
--- prove/Develop02/SaveToFile.cs
+++ prove/Develop02/SaveToFile.cs
@@ -1,2 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+
+public class SaveToFile
+{
+
+}
