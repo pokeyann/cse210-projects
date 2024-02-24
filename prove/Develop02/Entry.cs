47a7bfaaf38c97272375a803f8668ba9f3c4391c
Nathan Parrish <nvparrish@gatech.edu>
2024-02-23 22:59:28 -0500

    Changes to files in the call

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -5,13 +5,57 @@ using System.IO;
 
 public class Entry
 {
+  DateTime _date;
+  string _prompt;
+  string _response;
+
+  public Entry()
+  {
+    // Get a date
+    // Generate a prompt
+    // Get a response
+  }
+
+  public Entry(string prompt)
+  {
+    // Get a date
+    // Present the prompt
+    // Get a response
+  }
+
+  public Entry(DateTime date, string prompt, string response)
+  {
+
+  }
+
+  public Entry(string fileLine)
+  {
+    // Parse a line from the text file that saves the entries
+    List<string> data = fileLine.Split("%");
+    _date = DateTime.Parse(data[0]);
+    _prompt = data[1];
+    _response = data[2];
+  }
+
+  public string GenerateFileLine()
+  {
+    return $"{_date}%{_prompt}%{_response}"; 
+  }
+
+  public void Print()
+  {
+    string lineBreak = new String('=', 80);
+    Console.WriteLine($"{_date}\t{_prompt}\n\t{_response}{lineBreak}");
+  }
+
+
   public void journalEntry()
   {
     DateTime date = DateTime.Now;
     Console.WriteLine(date);
 
     Prompt prompt = new Prompt();
-    prompt.promptQuestions();
+    prompt.GetPrompt();
 
     Journal journal = new Journal();
     journal.journalEntry();
--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -3,11 +3,8 @@ using System.ComponentModel;
 using System.Net;
 public class Prompt
 {
-  public void promptQuestions()
-  {
 
-    List<string> questions = new List<string>
-    {
+  List<string> _questions = new List<string> {
       "What was the most surprising thing that happened today? ",
       "What am I most proud of? ",
       "What is my favorite animal and why? ",
@@ -16,27 +13,19 @@ public class Prompt
       "Where is your favorite place? ",
       "Describe your happiest childhood memory. ",
       "Filibuster for 5 minutes about your biggest pet peeve. "
-    };
+  };
 
-    Random prompt = new Random();
+  Random _random;
 
-    List<string> randomQuestions = new List<string>(); /*Wasn't working before because I needed an empty list to store the random selected questions*/
+  public Prompt()
+  {
+    _random = new Random();
+  }
 
-    for (int i = 0; i < 1; i++) /*wasn't working because it wasn't about selecting from 5 (i<5), the i++ goes through the whole list, 
-    need to set i<1 because only want one question at a time.*/
-    {
-      int questionsIndex = prompt.Next(0, questions.Count);
-      randomQuestions.Add(questions[questionsIndex]);
-    }
-
-    foreach (string prmpt in randomQuestions) /*could this code go in the for loop?  Too scared to break, don't want to try. not enough time to investigate.*/
-    {
-
-      Console.WriteLine(prmpt);
-      Console.Write("> ");
-    }
+  public string GetPrompt()
+  {
+    int questionsIndex = _random.Next(0, _questions.Count);
+    Console.WriteLine(_questions[questionsIndex]);
+    return _questions[questionsIndex];
   }
 }
-
-
-
