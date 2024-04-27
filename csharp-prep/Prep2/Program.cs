1bdc5bb3e0631ed615d2c7f02da1996ee4da6642
pokeyann <pokeyann@gmail.com>
2024-04-27 14:00:54 -0500

    prep2 step 1 else if

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+
+        else if (percent >= 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+        }
+
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+
+        else
+        {
+            Console.WriteLine("You grade is an F.");
+        }
     }
 }
