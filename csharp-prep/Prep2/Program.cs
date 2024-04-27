7153ea4338e2d8ba7e5c13fc8f531cf6382fbcba
pokeyann <pokeyann@gmail.com>
2024-04-27 14:15:03 -0500

    prep2 step 3 variable letter created single print statement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,30 +10,39 @@ class Program
         string userInput = Console.ReadLine();
         int percent = int.Parse(userInput);
 
+        string letter = "";
+
         if (percent >= 90)
         {
-            Console.WriteLine("Your grade is an A.");
+            //Console.WriteLine("Your grade is an A.");
+            letter = "A";
         }
 
         else if (percent >= 80)
         {
-            Console.WriteLine("Your grade is a B.");
+            //Console.WriteLine("Your grade is a B.");;
+            letter = "B";
         }
 
         else if (percent >= 70)
         {
-            Console.WriteLine("Your grade is a C.");
+            //Console.WriteLine("Your grade is a C.");
+            letter = "C";
         }
 
         else if (percent >= 60)
         {
-            Console.WriteLine("Your grade is a D.");
+            //Console.WriteLine("Your grade is a D.");
+            letter = "D";
         }
 
         else
         {
-            Console.WriteLine("You grade is an F.");
+            //Console.WriteLine("Your grade is an F.");
+            letter = "F";
         }
+
+        Console.WriteLine($"Your grade is: {letter}.");
 
         if (percent >= 70)
         {
