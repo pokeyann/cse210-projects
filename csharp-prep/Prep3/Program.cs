20cd37ae2c10fc58a722c60e3e7505a7fecb77b4
pokeyann <pokeyann@gmail.com>
2024-05-02 22:45:24 -0500

    magic number step 1 and 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        int magicNumber;
+        int guess;
+
+        Console.Write("What is the magic number? ");
+        magicNumber = int.Parse(Console.ReadLine());
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (magicNumber != guess);
     }
 }
