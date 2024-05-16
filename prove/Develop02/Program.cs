c25317fae44dbb2b87ba69d51087d9fac6ec2792
pokeyann <pokeyann@gmail.com>
2024-05-16 13:34:03 -0500

    Journal Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,79 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select from one of the following choices: ");
+
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load ");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+
+        Console.Write("What would you like to do? ");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine("Write");
+                break;
+
+            case "2":
+                Console.WriteLine("Display");
+                break;
+
+            case "3":
+                Console.WriteLine("Load");
+                break;
+
+            case "4":
+                Console.WriteLine("Save");
+                break;
+
+            case "5":
+                //Console.WriteLine("Quit");
+                System.Environment.Exit(0);
+                break;
+        }
+    }
+}
+
+public class Journal
+{
+    static void Main(string[] args)
+    {
+
+
+    }
+}
+
+public class Random //.Next error code so cannot check yet
+{
+    static void Main(string[] args)
+    {
+        Random randomPrompt = new Random();
+        //var prompts = new List<string>
+        string[] promptQuestions =
+        [
+            "What is your favorite dinosaur and why? ",
+            "What is the best advice you've ever recieved? ",
+            "Who is your celebrity crush? ",
+            "What is your least and most favorite chore? ",
+            "What are you most proud of? ",
+            "If you could change one thing, what would it be? ",
+            "What are your political beliefs? "
+        ];
+        //int index = random.Next(prompts.Count);
+        int index = randomPrompt.Next(promptQuestions.Length); //.Next is the syntax according to google searches??????
+        //Console.WriteLine(prompts[index]);
+        Console.WriteLine($"{promptQuestions[index]}");
+
+        //https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-8.0
     }
 }
