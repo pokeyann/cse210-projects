af397e718150ed0c9b303e4e46a5088a0c55f5c0
pokeyann <pokeyann@gmail.com>
2024-05-17 01:01:40 -0500

    create job.cs and instance job1 job2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Monkey Business";
+        job1._jobTitle = "Dance Monkey";
+        job1._startYear = 1997;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._company = "Beans";
+        job2._jobTitle = "Bean Counter";
+        job2._startYear = 2021;
+        job2._endYear = 2026;
+
+        Console.WriteLine($"{job1._jobTitle}, {job2._jobTitle}");
+
     }
 }
