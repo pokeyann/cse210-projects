a3201bb0eeacca6e50b80c2d7ae7a5dabd4e228c
pokeyann <pokeyann@gmail.com>
2024-06-07 01:16:47 -0500

    Create Reference class

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,5 +1,13 @@
 
 
+/*Scripture keeps track of the reference and text of the scripture. 
+Can hide words and get rendered display of text
+behaviors: Hide Words, Get Rendered Text , Is Completely Hidden
+Variables: reference, all the words in the scripture
+Constructor: accept both reference and text of scripture. 
+pass text of scripture as one string with all words, 
+then constructor creating list, 
+splitting  up the words in the string to create Word objects for each one and put them in a list*/
 class Scripture
 {
   private string book;
