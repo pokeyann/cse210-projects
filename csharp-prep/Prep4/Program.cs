bf336be74f82629ab176550bcac54dbdc626ef42
pokeyann <pokeyann@gmail.com>
2024-09-26 17:44:12 -0500

    working on differences between code and sample

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
@@ -24,7 +25,41 @@ class Program
         } while (userNum != 0);
 
 
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
 
-        Console.WriteLine($"The sum is: {}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
+        !!!!float average = ((float)userNum / numbers.Count);
+
+        Console.WriteLine($"The average is: {average}");
+
+
     }
+
+
 }
+
+/*
+Sample solutions uses foreach loops for the sum, avergae and largest number. 
+
+int sum = 0;
+foreach (number in numbers)
+{
+sum += number;
+}
+
+Console.WriteLine($"The sum is: {sum}");
+
+
+The following is how I did it based on the documentation about average for list 
+https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.average?view=net-5.0#system-linq-enumerable-average-1(system-collections-generic-ienumerable((-0))-system-func((-0-system-single)))
+        double average = numbers.Average();
+
+        Console.WriteLine($"The average is: {average}");
+The sample solution shows the following:
+float average = ((float)sum / numbers.Count);
+
+Console.WriteLine($"The average is: {average}");
+*/
