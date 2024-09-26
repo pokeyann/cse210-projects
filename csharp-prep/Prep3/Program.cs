ac0b89608009d1ad2f23bc4b29c9e8131d13a986
pokeyann <pokeyann@gmail.com>
2024-09-26 13:41:30 -0500

    step one if statements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
