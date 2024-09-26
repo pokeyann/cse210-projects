22d5da4623ef99c738318d634e43f07f49ee81e6
pokeyann <pokeyann@gmail.com>
2024-09-26 16:22:12 -0500

    create list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNum = -1;
+
+        Console.WriteLine("Enter a list of numbers, when done enter 0.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userNum = int.Parse(Console.ReadLine());
+
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+        } while (userNum != 0);
+
+
+
+        Console.WriteLine($"The sum is: {}");
     }
 }
