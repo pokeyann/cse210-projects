f995e491fd1e868fddf7105ab402572960d70b6d
pokeyann <pokeyann@gmail.com>
2024-09-26 21:56:31 -0500

    sum average max from list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -9,7 +9,7 @@ class Program
     {
         List<int> numbers = new List<int>();
 
-        int userNum = -1;
+        int userNum;
 
         Console.WriteLine("Enter a list of numbers, when done enter 0.");
 
@@ -24,25 +24,27 @@ class Program
             }
         } while (userNum != 0);
 
-
         Console.WriteLine($"The sum is: {numbers.Sum()}");
 
         Console.WriteLine($"The average is: {numbers.Average()}");
 
         Console.WriteLine($"The largest number is: {numbers.Max()}");
 
-        !!!!float average = ((float)userNum / numbers.Count);
-
-        Console.WriteLine($"The average is: {average}");
-
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
 
     }
-
-
 }
 
 /*
-Sample solutions uses foreach loops for the sum, avergae and largest number. 
+Sample solutions uses foreach loops for the sum and largest number and float for average. 
 
 int sum = 0;
 foreach (number in numbers)
@@ -52,14 +54,21 @@ sum += number;
 
 Console.WriteLine($"The sum is: {sum}");
 
-
-The following is how I did it based on the documentation about average for list 
+The following is another way I did based on the documentation about average for list 
 https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.average?view=net-5.0#system-linq-enumerable-average-1(system-collections-generic-ienumerable((-0))-system-func((-0-system-single)))
         double average = numbers.Average();
-
-        Console.WriteLine($"The average is: {average}");
-The sample solution shows the following:
-float average = ((float)sum / numbers.Count);
-
-Console.WriteLine($"The average is: {average}");
+         Console.WriteLine($"The average is: {average}");
+The sample solution shows the following, (but I get 0 when I try this.):
+    float average = ((float)userNum) / numbers.Count;
+    Console.WriteLine($"The average is: {average}");
+The sample solution using a foreach loop for max:
+int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
 */
