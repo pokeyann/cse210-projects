4d052f1565ed48abc5b62f88c178b2cfd636a414
pokeyann <pokeyann@gmail.com>
2024-10-03 13:34:38 -0500

    create job class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+public class Job
+{
+
+  public string _companyName = "";
+  public string _jobTitle = "";
+  public int _startYear = 0;
+  public int _endYear = 0;
+
+  public void DisplayJob()
+  {
+    Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}");
+  }
+}
+
+
