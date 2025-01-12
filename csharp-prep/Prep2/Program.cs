c717202f7b2fee6dcaccd5c20a1299cb76819192
pokeyann <pokeyann@gmail.com>
2025-01-11 21:46:22 -0600

    step 3 variable letter single print statement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,26 +10,30 @@ class Program
 
         int grade = int.Parse(Console.ReadLine());
 
+        string letter = "";
+
         if (grade >= 90)
         {
-            Console.WriteLine("Your grade is an A.");
+            letter = "A";
         }
         else if (grade >= 80)
         {
-            Console.WriteLine("Your grade is a B.");
+            letter = "B";
         }
         else if (grade >= 70)
         {
-            Console.WriteLine("Your grade is a C.");
+            letter = "C";
         }
         else if (grade >= 60)
         {
-            Console.WriteLine("Your grade is a D.");
+            letter = "D";
         }
-        else if (grade < 60)
+        else
         {
-            Console.WriteLine("Your grade is an F.");
+            letter = "F";
         }
+
+        Console.WriteLine($"Your grade is: {letter}.");
 
         if (grade >= 70)
         {
