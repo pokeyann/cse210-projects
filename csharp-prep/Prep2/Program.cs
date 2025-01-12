9ba102d2383d86844fbd28b70e0cec2b1418c2b0
pokeyann <pokeyann@gmail.com>
2025-01-11 21:34:37 -0600

    step 1 if else statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("Your grade is an F.");
+        }
     }
 }
