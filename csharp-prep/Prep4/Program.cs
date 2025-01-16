e178ab97fa534f3b2856079bfee30712bcb5c883
pokeyann <pokeyann@gmail.com>
2025-01-16 16:15:20 -0600

    step 1 sum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumbers = -1;
+
+        while (userNumbers != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumbers = int.Parse(Console.ReadLine());
+
+            numbers.Add(userNumbers);
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
+
     }
 }
