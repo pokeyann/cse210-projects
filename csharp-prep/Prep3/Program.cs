36133aedb10be60d2ad7e22e93d41c08257b55df
pokeyann <pokeyann@gmail.com>
2025-01-16 12:52:33 -0600

    step 1 if statement

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (number < guess)
+        {
+            Console.WriteLine("lower");
+        }
+
+        else
+        {
+            Console.WriteLine("higher");
+        }
+
     }
 }
