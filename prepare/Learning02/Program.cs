667c75e05584d2834776411cc1d927d737be2550
pokeyann <pokeyann@gmail.com>
2025-01-22 14:41:04 -0600

    through step 4

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Master of the Universe";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Power of Grayskull";
+
+        Console.WriteLine($"{job1._jobTitle}");
+
+        Console.WriteLine($"{job2._jobTitle}");
     }
+
 }
