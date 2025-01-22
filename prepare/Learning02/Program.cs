88b3843b3bd00632f46821d165a320a252670dab
pokeyann <pokeyann@gmail.com>
2025-01-22 14:46:29 -0600

    step 5 display method

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,13 +8,18 @@ class Program
 
         Job job1 = new Job();
         job1._jobTitle = "Master of the Universe";
+        job1._company = "He-Man";
+        job1._startYear = 1983;
+        job1._endYear = 1985;
 
         Job job2 = new Job();
         job2._jobTitle = "Power of Grayskull";
+        job2._company = "Mattel";
+        job2._startYear = 2002;
+        job2._endYear = 2004;
 
-        Console.WriteLine($"{job1._jobTitle}");
-
-        Console.WriteLine($"{job2._jobTitle}");
+        job1.Display();
+        job2.Display();
     }
 
 }
