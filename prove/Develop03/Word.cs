8a19b7da0275b01f5ecfda23153d8976d186cbad
pokeyann <pokeyann@gmail.com>
2025-02-13 13:43:45 -0600

    notes for word

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -4,3 +4,23 @@ class Word
 {
 
 }
+
+/*
+book = Proverbs
+chapter = 3
+startVerse = 5
+endVerse = 6
+words = Trust in the Lord with all thine heart and lean not unto thine own understanding;
+in all the ways acknowledge him, and he shall direct they paths.
+
+loop through and console clear until no all words console cleared or type quit
+
+Scripture scripture = new Scripture();
+
+        bool loopContinue = true;
+        while (loopContinue)
+        {
+            Console.WriteLine(""); book chapter startverse endverse text
+            console clear 
+            should this be in word and called here?  should the loop be in word?*/
+        }
