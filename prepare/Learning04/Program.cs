18033fddd9e5617f47f6d5dca2cb6e7cf46ac2f5
pokeyann <pokeyann@gmail.com>
2025-03-03 16:15:25 -0600

    step 3

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new Assignment();
+        assignment1.SetStudentName("Samuel Bennett");
+        assignment1.SetTopic("Multiplication");
+
+        Console.WriteLine(assignment1.GetSummary());
     }
 }
