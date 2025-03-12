83fa6b4c5edeb81b0106b408ee5676ccae90f76b
pokeyann <pokeyann@gmail.com>
2025-03-12 15:18:44 -0500

    general outline

--- prove/Develop04/Base.cs
+++ prove/Develop04/Base.cs
@@ -2,5 +2,30 @@ using System;
 
 class Base
 {
+  private int _seconds;
+  public void StartMessage()
+  {
+    Console.WriteLine("Welcome to the      Activity.");
+    Console.WriteLine("This activity will help you      ");
+    Console.WriteLine("How long in seconds, would you like your session? ");
 
+    _seconds = int.Parse(Console.ReadLine());
+  }
+
+  public void EndMessage()
+  {
+    Console.WriteLine("Well Done!");
+    Console.WriteLine($"You have completed       of the      ");
+  }
+
+  public void SpinnerAnimation()
+  {
+    Console.Write("Get ready... ");
+  }
+
+  public void CountdownAnimation()
+  {
+    Console.WriteLine("You may begin in: ");
+
+  }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -14,6 +14,7 @@ class Program
             Console.WriteLine("2. Start Reflecting Activity");
             Console.WriteLine("3. Start Listing Activity");
             Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu: ");
 
             int userChoice = int.Parse(Console.ReadLine());
 
