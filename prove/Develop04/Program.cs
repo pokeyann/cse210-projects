7356fb56774b7b4fff447311f13c847071108013
pokeyann <pokeyann@gmail.com>
2025-03-12 14:57:23 -0500

    main set up

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        bool loopContinue = true;
+        while (loopContinue)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflecting Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+
+            int userChoice = int.Parse(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    /*Quit();*/
+                    break;
+            }
+        }
     }
 }
